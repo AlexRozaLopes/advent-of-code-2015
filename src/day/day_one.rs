a332dab6ed18c9ba80dce4d4449fdf652e096711
lopes <alexrozaestudos@gmail.com>
2025-11-25 14:37:43 -0300

    Day 1 Part 2 OK!

--- src/day/day_one.rs
+++ src/day/day_one.rs
@@ -8,3 +8,19 @@ pub fn what_floor(input: &str) -> i32 {
             }
     })
 }
+
+pub fn first_basement(input: &str) -> usize {
+    input
+        .chars()
+        .enumerate()
+        .scan(0, |floor, (i, c)| {
+            *floor += match c {
+                '(' => 1,
+                ')' => -1,
+                _ => 0,
+            };
+            Some((i, *floor))
+        })
+        .find(|(_, floor)| *floor == -1)
+        .map(|(i, _)| i + 1).unwrap()
+}
--- src/main.rs
+++ src/main.rs
@@ -6,5 +6,10 @@ fn main() {
     println!(
         "Day 1: Not Quite Lisp: PART ONE - ANSWER {}",
         day_one::what_floor(include_str!("data/day-one"))
+    );
+
+    println!(
+            "Day 1: Not Quite Lisp: PART TWO - ANSWER {}",
+        day_one::first_basement(include_str!("data/day-one"))
     )
 }
